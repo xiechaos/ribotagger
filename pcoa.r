dd915e103f9428d296e741306519eb740451b08d
etheleon <picy2k@gmail.com>
2015-03-14 00:40:48 +0800

    run command: Rscript -e "library(knitr); opts_knit\$set(root.dir=getwd()); spin('../pcoa.r')" out/my.sample.tab out/my.sample.pcoa knit To generate report. However Figure captions are missing!

--- pcoa.r
+++ pcoa.r
@@ -1,28 +1,43 @@
 #!/usr/bin/env Rscript
 
+#+ Library, echo=FALSE, message=FALSE, warning=FALSE
 library(vegan)
 library(ggplot2)
 theme_set(theme_bw())
+invisible(dev.off())
 
+#+ Arguments, echo=FALSE
 args <- commandArgs(T);
-
-if(length(args) != 2)
-{
-	cat("usage: pcoa.r ribotag.tab output.dir\n");
-	q()
+if(args[3] == 'knit'){
+    in.file <- args[1]
+    out.dir <- args[2]
+    #in.file <- 'out/my.sample.tab'
+    #out.dir <- 'out/my.sample.pcoa'
+}else{
+    if(length(args) < 2)
+    {
+        cat("usage: pcoa.r ribotag.tab output.dir\n");
+        q()
+    }else{
+        in.file <- args[1]
+        out.dir <- args[2]
+    }
 }
 
-in.file <- args[1]
-out.dir <- args[2]
+
+#+ echo=FALSE
 dir.create(out.dir)
-top <- as.numeric(args[3]) # 0..1, default 1
-factor <- as.numeric(args[4]) # default 10
+top                      <- as.numeric(args[4])      # 0..1, default 1
+factor                   <- as.numeric(args[5])      # default 10
 if(is.na(factor)) factor <- 10
 
-table <- read.delim(in.file, check.names = F)
+#+ readData, echo=FALSE
+table           <- read.delim(in.file, check.names = F)
 rownames(table) <- as.character(table[, 1])
-table <- table[, -1, drop = F]
+table           <- table[, -1, drop = F]
 
+
+#+ SampleCheck, echo=FALSE
 ncols <- ncol(table)
 if(ncols < 3)
 {
@@ -30,6 +45,7 @@ if(ncols < 3)
 	q()
 }
 
+#+ echo=FALSE
 prop <- apply(table, 2, function(v) 100 * v / sum(v))
 
 if(!is.na(top))
@@ -42,16 +58,12 @@ if(!is.na(top))
 }
 
 data <- prop
-
 di <- vegdist(t(data), method = 'bray')
-
 kdim <- ifelse(ncols <= 3, 2, 3)
-
 pcoa <- cmdscale(di, k = kdim, eig = T)
 pco <- data.frame(pcoa$points)
 pco$sample <- rownames(pco)
 eig <- pcoa$eig
-
 		Y <- t(data)
 		plot.axes <- 1:kdim
         n <- nrow(Y)
@@ -69,43 +81,48 @@ eig <- pcoa$eig
 		U[abs(U$X1) >= max(abs(pco$X1))/factor, 'good'] <- T
 		U[abs(U$X2) >= max(abs(pco$X2))/factor, 'good'] <- T
 		if(kdim == 3) U[abs(U$X3) >= max(abs(pco$X3))/factor, 'good'] <- T
-
 explained <- 100 * eig / sum(eig)
 
-pdf(sprintf('%s/pcoa.12.pdf', out.dir))
-qplot(X1, X2, data = pco, label = sample, geom = 'text', size = I(4)) + 
+#+ pcoa.12, echo=FALSE
+oneVtwo = qplot(X1, X2, data = pco, label = sample, geom = 'text', size = I(4)) + 
 	scale_x_continuous(sprintf("MDS1 (%.2f%%)", explained[1])) + 
 	scale_y_continuous(sprintf("MDS2 (%.2f%%)", explained[2]))  
-dev.off()
+ggsave(oneVtwo, file=sprintf('%s/pcoa.12.pdf', out.dir))
+oneVtwo
 
+#+ pcoa.13,echo=FALSE
 if(kdim == 3)
 {
-	q <- qplot(X1, X3, data = pco, label = sample, geom = 'text', size = I(4)) +
+	oneVthree <- qplot(X1, X3, data = pco, label = sample, geom = 'text', size = I(4)) +
 		scale_x_continuous(sprintf("MDS1 (%.2f%%)", explained[1])) + 
 		scale_y_continuous(sprintf("MDS3 (%.2f%%)", explained[3]))  
-	pdf(sprintf('%s/pcoa.13.pdf', out.dir))
-	print(q)
-	dev.off()
+	ggsave(oneVthree, file=sprintf('%s/pcoa.13.pdf', out.dir))
+    oneVthree
 }
 
-pdf(sprintf('%s/pcoa.biplot.12.pdf', out.dir))
-ggplot(pco, aes(X1, X2)) + geom_text(aes(label = sample), size = 4)+ 
+#+ pcoa.biplot.12,echo=FALSE
+biplot = ggplot(pco, aes(X1, X2)) + geom_text(aes(label = sample), size = 4)+ 
 	scale_x_continuous(sprintf("MDS1 (%.2f%%)", explained[1])) + 
 	scale_y_continuous(sprintf("MDS2 (%.2f%%)", explained[2])) +
 	geom_segment(aes(xend = 0, yend = 0), U) +
 	geom_text(aes(label = tag), subset(U, good), size = 1.5)
-dev.off()
+ggsave(biplot, file=sprintf('%s/pcoa.biplot.12.pdf', out.dir))
+biplot
 
+#+ pca.biplot.13,echo=FALSE
 if(kdim == 3)
 {
-	q <- ggplot(pco, aes(X1, X3)) + geom_text(aes(label = sample), size = 4)+ 
+	biplot2 <- ggplot(pco, aes(X1, X3)) + geom_text(aes(label = sample), size = 4)+ 
 		scale_x_continuous(sprintf("MDS1 (%.2f%%)", explained[1])) + 
 		scale_y_continuous(sprintf("MDS3 (%.2f%%)", explained[3])) + 
 		geom_segment(aes(xend = 0, yend = 0), U) +
 		geom_text(aes(label = tag), subset(U, good), size = 1.5)
-	pdf(sprintf('%s/pcoa.biplot.13.pdf', out.dir))
-	print(q)
-	dev.off()
+    ggsave(biplot2, file=sprintf('%s/pcoa.biplot.13.pdf', out.dir))
+    biplot2
 }
 
+#+ saveData, echo=FALSE
 save(data, pco, pcoa, U, explained, file = sprintf('%s/data.rda', out.dir))
+
+# echo=FALSE
+sessionInfo()
